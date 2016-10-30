ba5c2b739c633d7e6cda46015d7072c582c4fda7
plasne <plasne@Peters-iMac.local>
2016-10-30 10:14:26 -0400

    redesigned

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
+
+// includes
+var fs = require("fs");
 var express = require("express");
 var service = require("./lib/service.js");
+var region = require("./lib/region.js");
+var condition = require("./lib/condition.js");
 
+// globals
 var app = express();
 
+// read the configuration files
+fs.readdir("./config", function(error, files) {
+    if (!error) {
+
+        // load the regions
+        region.load(files).then(function() {
+            service.load(files).then(function() {
+                condition.load(files).then(function() {
+                    console.log("done loading...");
+                });
+            });
+        });
+
+        //console.log(files);
+    } else {
+        throw error;
+    }
+});
+
+/*
 var s = service.new({
     name: "bob",
     priority: 40,
@@ -13,7 +39,7 @@ var s = service.new({
         },
         results: [
             {
-                "responses": [ "200", "201-206" ],
+                "response": "200-206",
                 "state": "up"
             },
             {
@@ -22,11 +48,13 @@ var s = service.new({
         ]
     }
 });
+*/
 
 app.get("/", function(req, res) {
     res.send("hello");
 });
 
+// startup the web services
 app.listen(80, function() {
     console.log("listening on port 80...");
 });
