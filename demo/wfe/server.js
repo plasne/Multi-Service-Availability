8e9ff119646f4018c2feaad47ee417704c2428a9
plasne <plasne@Peters-iMac.local>
2016-11-16 17:05:43 -0500

    added proxy

--- demo/wfe/server.js
+++ demo/wfe/server.js
@@ -1,12 +1,26 @@
+
 var express = require("express");
+var request = require("request");
 var app = express();
 
 app.use(express.static("files"));
 
 app.get("/", function(req, res) {
-  res.redirect("/default.htm");
+    res.redirect("/default.htm");
+});
+
+app.get("/name", function(req, res) {
+    request({
+        uri: "http://app/name"
+    }, function(error, response, body) {
+        if (!error) {
+            res.status(response.statusCode).send(body);
+        } else {
+            res.status(500).send(error);
+        }
+    });
 });
 
 app.listen(80, function() {
-  console.log("listening on port 80...");
+    console.log("listening on port 80...");
 });
