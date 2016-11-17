0624e038aa6deef1f6339298a44beb6f1a4ba85f
plasne <plasne@Peters-iMac.local>
2016-11-16 20:01:11 -0500

    docker rules

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 
+// TODO:
+//  look at why scaling up sometimes changes the master (maybe has something to do with order of instances)
+//  when calling /all/instance, if that instance is no longer available show local
+
+
 // includes
 const verror = require("verror");
 const util = require("util");
