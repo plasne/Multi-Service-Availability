2c00aa65cd5cdc4f430f1a0ca8c2c1ddbc6058f7
plasne <plasne@Peters-iMac.local>
2016-11-16 20:44:26 -0500

    fixed

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 //  look at why scaling up sometimes changes the master (maybe has something to do with order of instances)
 //  when calling /all/instance, if that instance is no longer available show local
 //  proxy doesn't work across regions (bounces around)
+//  remote region bounces between master and slave
 
 // includes
 const verror = require("verror");
